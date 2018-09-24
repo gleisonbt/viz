01698691b1da4b22900b9fbe48c258d8967b5f45
gleisonbt <brittobaptista93@gmail.com>
2018-09-24 16:01:45 -0300

    wrapper graphql

--- githubstats/github_stats.py
+++ githubstats/github_stats.py
@@ -25,6 +25,8 @@ import geocoder
 
 import requests
 from requests.auth import HTTPBasicAuth
+from githubstats.wrapper_graphQL import *
+
 
 from githubstats.lib.github import GitHub
 from githubstats.repo import Repo
@@ -145,22 +147,6 @@ class GitHubStats(object):
         self.CFG_USERS_GEOCODES_PATH = self.build_module_path(
             'data/2017/users_geocodes')
 
-    def run_query(self, query):
-        URL = 'https://api.github.com/graphql'
-
-        headers = {"Authorization": "Bearer abdd103184f1537d2208ddddb2fc26e12c453895"}
-
-        request = requests.post(URL, json=query,headers=headers)
-
-        #print(self.github.user_pass)
-        #print(self.github.user_login)
-
-        #request = requests.post(URL, json=query,auth=HTTPBasicAuth("gleisonbt", "Aleister93"))
-
-        if request.status_code == 200:
-            return request.json()
-        else:
-            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
     def build_module_path(self, path):
         """Builds the path relative to where the module is loaded.
@@ -332,71 +318,11 @@ class GitHubStats(object):
         self.overall_orgs.extend(orgs)
 
     def user_type(self, item_id):
-        queryType="""
-                    query type($queryUser:String!){
-                        search(query:$queryUser, type:USER, first:100){
-                            nodes{
-                                ... on Actor{
-                                __typename
-                            }
-                        }
-                    }
-                    }
-                    """
-        jsonType = {
-            "query": queryType, "variables":{
-            "queryUser": "user:" + item_id
-            }
-        }
-
-        result = self.run_query(jsonType)
+        return user_type(self,item_id)
         
-        return result["data"]["search"]["nodes"][0]["__typename"]
-    
     def user_data(self, item_id, ):
-
-        item_type = self.user_type(item_id)
-
-        prefixQueryUser = """
-            query findUser($user:String!){
-                user(login:$user){
-        """
-
-        prefixQueryOrganization = """
-            query findOrganization($user:String!){
-            organization(login:$user){
-        """
-
-        prefix = None
-        if item_type == "User":
-            prefix = prefixQueryUser
-        else:
-            prefix = prefixQueryOrganization
-
-        query =  prefix + """
-                    name
-                    location
-                }
-                }    
-        """
-
-        json = {
-            "query":query, "variables":{
-                "user":item_id
-            }
-        }
-
-        result = self.run_query(json)
+        return user_data(self,item_id)
         
-        #print(result)
-
-        if item_type == "User":
-            return {"name":result["data"]["user"]["name"],
-            "location":result["data"]["user"]["location"], "type":item_type}
-        else:
-            return {"name":result["data"]["organization"]["name"],
-            "location":result["data"]["organization"]["location"], "type":item_type}
-
 
     def get_user_info(self, sorted_users):
         """Gets user info from the GitHubAPI.
@@ -600,82 +526,11 @@ class GitHubStats(object):
     def print_rate_limit(self):
         """Prints the rate limit."""
 
-        query = """
-             query { 
-                 rateLimit{
-                     remaining
-                 }
-             }
-         """
-
-        json = {
-             "query":query, "variables":{}
-        }
-
-        result = self.run_query(json)["data"]["rateLimit"]["remaining"]
-
-        click.echo('Rate limit: ' + str(result))
+        print_rate_limit(self)
 
 
     def search_repositories(self, query, sort='stars'):
-        queryRepos = """
-            query findRepos($query:String!){
-            search(query:$query, type:REPOSITORY, first:100{AFTER}){
-                pageInfo{
-                hasNextPage
-                endCursor
-                }
-                nodes{
-                ... on Repository{
-                    nameWithOwner
-                    stargazers{
-                    totalCount
-                    }
-                    forks{
-                    totalCount
-                    }
-                    description
-                    primaryLanguage{
-                    name
-                    }
-                }
-                }
-            }
-            rateLimit{
-                remaining
-                resetAt
-            }
-            }    
-        """
-
-        fistQuery = queryRepos.replace("{AFTER}", "")
-
-        json = {
-            "query":fistQuery, "variables":{
-                "query":query + " sort:" + sort
-            }
-        }
-
-        result = self.run_query(json)
-
-        nodes = result["data"]["search"]["nodes"]
-
-        next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
-        while next_page:
-            cursor = result["data"]["search"]["pageInfo"]["endCursor"]
-            next_query = queryRepos.replace("{AFTER}", ", after: \"%s\"" % cursor)
-            json["query"] = next_query
-            result = self.run_query(json)
-            nodes += result["data"]["search"]["nodes"]
-            next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
-        
-        repositories = []
-        for node in nodes:
-            repositories.append(Repo(node["nameWithOwner"], node["stargazers"]["totalCount"],
-                     node["forks"]["totalCount"], node["description"], node["primaryLanguage"]["name"]))
-
-
-        return repositories
+        return search_repositories(self, query, sort='stars')
 
 
 
@@ -907,3 +762,4 @@ class GitHubStats(object):
         csv_path = self.build_module_path('data/2017/user-geocodes-dump.csv')
         user_geocoder.generate_user_geocodes(csv_path,
                                              self.CFG_USERS_GEOCODES_PATH)
+
